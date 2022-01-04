f89e48f05187b6751c8d91375a78ec06620363fd
youssef hassan <youssefhassan369@yahoo.com>
2022-01-04 04:54:26 +0200

    v

---
+++ DB connection.php
@@ -0,0 +1,5 @@
+<?php
+$connection = mysqli_connect("127.0.0.1","root","","carRentalDB");
+ if (!$connection) {
+     die("Connection failed: " . mysqli_connect_error());
+ }
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include "DB connection.php";
+
+$email=$_POST['email'];
+$password=$_POST['password'];
+$encrypted_password=md5($password);
+if($email!=''&&$password!='')
+ {
+   $query=mysqli_query($connection,"SELECT * 
+                                    FROM customer  
+                                    WHERE email='$email' AND password='$encrypted_password'") ;
+   $res=mysqli_fetch_array($query);
+   if($res){
+   $query = "SELECT * FROM user WHERE email = '$email'";
+    $result = mysqli_query($connection, $query);
+    $userRow = mysqli_fetch_array($result);
+    $fname = $userRow['fname'];
+    $lname = $userRow['lname'];
+    $email = $userRow['email'];
+    $_SESSION['email']=$email;
+    $_SESSION['fname']=$fname;
+    $_SESSION['lname']=$lname;
+    header('location:welcome.php');
+    echo"vaild";
+   }
+   else
+   {
+    echo"Invaild email or password";
+   }
+ }
+
+ else{
+    echo"please enter email and password";
+ }
